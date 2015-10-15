a4be6e809e072e55d4e78de4d805dd737501d1da
AryanSingh <aryan@iitgn.ac.in>
2015-10-15 12:17:27 +0530

    adding static page controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,7 @@
+class StaticPagesController < ApplicationController
+  def add
+  end
+
+  def done
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/add'
+
+  get 'static_pages/done'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
