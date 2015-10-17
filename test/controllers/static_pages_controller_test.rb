80dc1a3ccec84d9d7d43e9792d8b0d5e4165faf4
AryanSingh <aryan@iitgn.ac.in>
2015-10-17 14:15:03 +0530

    finished the exercises

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on rails ToDo App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+
   test "should get add" do
     get :add
     assert_response :success
-    assert_select "title", "Add | Ruby on rails ToDo App"
+    assert_select "title", "Add | #{@base_title}"
   end
 
   test "should get done" do
     get :done
     assert_response :success
-    assert_select "title", "Done | Ruby on rails ToDo App"
+    assert_select "title", "Done | #{@base_title}"
   end
 
   test "should get delete" do
     get :delete
     assert_response :success
-    assert_select "title", "Delete | Ruby on rails ToDo App"
+    assert_select "title", "Delete | #{@base_title}"
   end
 
 end
